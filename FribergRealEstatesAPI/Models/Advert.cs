8e109e9573d4a88e9591cc0a18794d407231aef0
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-15 15:57:25 +0200

    added Advert class

---
+++ FribergRealEstatesAPI/Models/Advert.cs
@@ -0,0 +1,14 @@
+﻿namespace FribergRealEstatesAPI.Models
+{
+    // Created by Robert
+    public class Advert
+    {
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public bool Sold { get; set; }
+        public Realtor Realtor { get; set; }
+        public int RealtorId { get; set; }
+        public Residence Residence { get; set; }
+        public int ResidenceId { get; set; }
+    }
+}
