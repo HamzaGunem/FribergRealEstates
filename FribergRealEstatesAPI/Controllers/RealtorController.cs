de6a2686daa52ed16287ebbc32616da230e49a25
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-16 14:35:50 +0200

    added realtorcontroller and repository and injected as service in program

---
+++ FribergRealEstatesAPI/Controllers/RealtorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergRealEstatesAPI.Controllers
+{
+    // Created By Robert
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealtorController : ControllerBase
+    {
+    }
+}
---
+++ FribergRealEstatesAPI/Data/Repositories/RealtorRepository.cs
@@ -0,0 +1,13 @@
+﻿using FribergRealEstatesAPI.Data.Interfaces;
+using FribergRealEstatesAPI.Models;
+
+namespace FribergRealEstatesAPI.Data.Repositories
+{
+    // Created by Robert
+    public class RealtorRepository : GenericRepository<Realtor, ApiDbContext>, IRealtorRepository
+    {
+        public RealtorRepository(ApiDbContext context) : base(context)
+        {
+        }
+    }
+}
