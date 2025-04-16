d912344fc882820362bf15b27b6b6c3aac01bf19
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-16 16:40:34 +0200

    Added funktionaly HTTPGET , added query in realtorrepo, added realtoradversdto

--- FribergRealEstatesAPI/Data/Repositories/RealtorRepository.cs
+++ FribergRealEstatesAPI/Data/Repositories/RealtorRepository.cs
@@ -13,11 +13,18 @@ namespace FribergRealEstatesAPI.Data.Repositories
 
         public async Task<IEnumerable<Advert>> GetActiveAdvertsByRealtorIdAsync(int realtorId)
         {
-            return await _context.Adverts
-                .Include(r => r.Residence)
-                .Include(a => a.Realtor)
-                .Where(a => a.RealtorId == realtorId && !a.Sold)
-                .ToListAsync();
+            var realtor = await _context.Realtors
+                .Include(b => b.Agency)
+                .Include(r => r.ActiveAdverts)
+                .ThenInclude(l => l.Residence)
+                .ThenInclude(a => a.Address)
+                .ThenInclude(c => c.Commun)
+                .FirstOrDefaultAsync(r => r.Id == realtorId);
+
+            if (realtor == null || realtor.ActiveAdverts == null)
+                return Enumerable.Empty<Advert>();
+
+            return realtor.ActiveAdverts.Where(a => !a.Sold).ToList();
         }
     }
 }
