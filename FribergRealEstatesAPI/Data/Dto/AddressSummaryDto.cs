b073903981ef509c16d93ceee77797bede2ee4b8
SamAndrs <sam_andrs@yahoo.se>
2025-04-17 17:55:27 +0200

    update2

---
+++ FribergRealEstatesAPI/Data/Dto/AddressSummaryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FribergRealEstatesAPI.Data.Dto
+{
+    //Auth: Hamza
+    public class AddressSummaryDto
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
