b45b6ae5a4292ffc1435535a364f06945ed25711
SamAndrs <sam_andrs@yahoo.se>
2025-04-17 17:44:44 +0200

    updated

--- FribergRealEstatesAPI/Data/Dto/ResidenceDto.cs
+++ FribergRealEstatesAPI/Data/Dto/ResidenceDto.cs
@@ -1,22 +1,19 @@
-﻿namespace FribergRealEstatesAPI.Data.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergRealEstatesAPI.Data.Dto
 {
-    //Auth: Hamza
-    public class ResidenceDto
+    public class ResidenceDTO
     {
         public int Id { get; set; }
-        public int Area { get; set; }
-        public int? BiArea { get; set; }
+
+        public int Area { get; set; } // Main living area
+
+        public int? BiArea { get; set; } // Extra Area
+
+        [Length(250, 500)]
         public string Description { get; set; }
+
         public int Rooms { get; set; }
-        public double OperatingCost { get; set; }
-        public int FloorRows { get; set; } // Total floors inside apartment/house
-        public double? MonthlyFee { get; set; }
-        public int BuildYear { get; set; }
 
-        public string[]? ImageUrls { get; set; }
-
-        public int? ParkingSlotNumber { get; set; }
-
-        public AddressDto Address { get; set; }
     }
 }
