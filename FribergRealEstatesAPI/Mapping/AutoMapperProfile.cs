5754ddd4b1869733d377f4089b300895f5ea41c0
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-22 11:19:13 +0200

    added automapper for realtorprofile

--- FribergRealEstatesAPI/Mapping/AutoMapperProfile.cs
+++ FribergRealEstatesAPI/Mapping/AutoMapperProfile.cs
@@ -25,6 +25,10 @@ namespace FribergRealEstatesAPI.Mapping
                 .ForMember(rdto => rdto.ActiveAdverts, opt => opt.MapFrom(r => r.ActiveAdverts))
                 .ReverseMap();
 
+            // Realtor-profile by Robert
+            CreateMap<Realtor, RealtorProfileDto>()
+                .ForMember(rdto => rdto.AgencyName, opt => opt.MapFrom(r => r.Agency.Name));
+
           // Realtor-Adverts by Robert
             CreateMap<Advert, RealtorAdvertsDto>()
             .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created))
@@ -45,13 +49,13 @@ namespace FribergRealEstatesAPI.Mapping
                 .ForMember(adto => adto.CommunName, opt => opt.MapFrom(a => a.Commun.Name))
                 .ReverseMap();
 
-
             //Default mapping with no reference to any other class
             CreateMap<Realtor, RealtorSummaryDto>();
             CreateMap<Residence, ResidenceSummaryDto>();
             CreateMap<Advert, AdvertSummaryDto>();
             CreateMap<Address, AddressDto>();
             CreateMap<Address, AddressSummaryDto>(); // Samuel
+          
         }
     }
 }
