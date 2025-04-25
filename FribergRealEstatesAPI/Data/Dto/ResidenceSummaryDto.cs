9715fcf0c2daec9a967eacebdfdff23d8dd703e3
hamza gneum <hamza.gunem@hotmail.com>
2025-04-25 09:04:19 +0200

    ResidenceSummaryDto fix

--- FribergRealEstatesAPI/Data/Dto/ResidenceSummaryDto.cs
+++ FribergRealEstatesAPI/Data/Dto/ResidenceSummaryDto.cs
@@ -8,6 +8,7 @@ namespace FribergRealEstatesAPI.Data.Dto
         public int Id { get; set; }
         public int Area { get; set; }
         public int? BiArea { get; set; }
+        public string Description { get; set; }
         public int Rooms { get; set; }
         public double OperatingCost { get; set; }
         public double? MonthlyFee { get; set; }
