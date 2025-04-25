5915ab44f9efe39127fcda4d91ca943afc89eecd
hamza gneum <hamza.gunem@hotmail.com>
2025-04-26 01:19:39 +0200

    JsonPropertyOrder for soldadverts and activeadverts

--- FribergRealEstatesAPI/Data/Dto/RealtorFullProfileDto.cs
+++ FribergRealEstatesAPI/Data/Dto/RealtorFullProfileDto.cs
@@ -4,9 +4,14 @@ namespace FribergRealEstatesAPI.Data.Dto
 {
     public class RealtorFullProfileDto
     {
-        [JsonIgnore]
         public RealtorSummaryDto Realtor { get; set; }
+
+        [JsonPropertyOrder(1)]
+        [JsonPropertyName("activeAdverts")]
         public List<AdvertDto>? ActiveAdverts { get; set; }
+
+        [JsonPropertyOrder(2)]
+        [JsonPropertyName("soldAdverts")]
         public List<AdvertDto>? SoldAdverts { get; set; }
 
     }
