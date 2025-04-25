5906f88dd13b7fb47aa8712cd11d0ee3a476450b
hamza gneum <hamza.gunem@hotmail.com>
2025-04-25 19:31:48 +0200

    Datanotations for realtorfullprofile

--- FribergRealEstatesAPI/Data/Dto/RealtorFullProfileDto.cs
+++ FribergRealEstatesAPI/Data/Dto/RealtorFullProfileDto.cs
@@ -3,7 +3,9 @@
     public class RealtorFullProfileDto
     {
         public RealtorSummaryDto Realtor { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("activeAdverts")]
         public List<AdvertDto> ActiveAdverts { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("soldAdverts")]
         public List<AdvertDto> SoldAdverts { get; set; }
 
     }
