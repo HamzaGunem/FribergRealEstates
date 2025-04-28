e829ef672e1e16a813d27277fbc179dae5726998
SamAndrs <sam_andrs@yahoo.se>
2025-04-28 14:58:34 +0200

    update 2

--- FribergRealEstatesAPI/Data/ApiDbContext.cs
+++ FribergRealEstatesAPI/Data/ApiDbContext.cs
@@ -1,4 +1,6 @@
-﻿using FribergRealEstatesAPI.Models;
+﻿using FribergRealEstatesAPI.Constants;
+using FribergRealEstatesAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -50,6 +52,47 @@ namespace FribergRealEstatesAPI.Data
                 .WithMany(a => a.Realtors)
                 .HasForeignKey(r => r.AgencyId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+            {
+                Name = ApiRoles.User,
+                NormalizedName = ApiRoles.User,
+                Id = "",
+            },
+                new IdentityRole
+                {
+                    Name = ApiRoles.Admin,
+                    NormalizedName = ApiRoles.Admin,
+                    Id = "",
+                },
+                new IdentityRole
+                {
+                    Name = ApiRoles.SuperAdmin,
+                    NormalizedName = ApiRoles.SuperAdmin,
+                    Id = "",
+                },
+                new IdentityRole
+                {
+                    Name = ApiRoles.Realtor,
+                    NormalizedName = ApiRoles.Realtor,
+                    Id = "",
+                });
+
+            var hasher = new PasswordHasher<ApiUser>();
+            modelBuilder.Entity<ApiUser>().HasData(
+                new ApiUser
+                {
+                    Id = "",
+                    Email = "admin@api.com",
+                    NormalizedEmail = "ADMIN@API.COM",
+                    UserName = "admin@api.com",
+                    NormalizedUserName = "ADMIN@API.COM",
+                    FirstName = "System",
+                    LastName = "Admin",
+                    PasswordHash = hasher.HashPassword(null, "Admin123!"),
+                    EmailConfirmed = true
+                });
         }
     }
 }
