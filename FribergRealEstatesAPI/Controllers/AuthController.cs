a647d69c571bf92b6b74435212d4da5b5e15717d
Viktor Hansson <viha2303@student.miun.se>
2025-04-28 14:29:57 +0200

    Added register

---
+++ FribergRealEstatesAPI/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using FribergRealEstatesAPI.Constants;
+using FribergRealEstatesAPI.Data;
+using FribergRealEstatesAPI.Data.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergRealEstatesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+
+        public AuthController(UserManager<ApiUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
+            {
+                ApiUser user = new ApiUser()
+                {
+                    UserName = userDto.Email,
+                    Email = userDto.Email,
+                    FirstName = userDto.FirstName,
+                    LastName = userDto.LastName
+                };
+
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+
+                if (result.Succeeded == false)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+
+                await _userManager.AddToRoleAsync(user, ApiRoles.User);
+                return Accepted();
+            }
+            catch (Exception ex)
+            {
+                return Problem($"Something Went Wrong in the {nameof(Register)}", statusCode : 500);
+            }
+        }
+    }
+}
---
+++ FribergRealEstatesAPI/Data/Dto/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergRealEstatesAPI.Data.Dto
+{
+    public class UserDto
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
