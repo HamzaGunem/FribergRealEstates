0b55031838d36d39c408e54744fe798ee695ee98
SamAndrs <sam_andrs@yahoo.se>
2025-05-01 08:19:23 +0200

    Changed returntype RealtorDto to RealtorSummaryDto on GetRealtorsByAgencyCommunName endpoint

--- FribergRealEstatesAPI/Controllers/RealtorController.cs
+++ FribergRealEstatesAPI/Controllers/RealtorController.cs
@@ -59,14 +59,14 @@ namespace FribergRealEstatesAPI.Controllers
             return Ok(response);
         }
 
-        //Hamza
+        //Hamza, return type changed by Samuel
         [HttpGet("{communName}/realtors/byCommun")]
-        public async Task<ActionResult<List<RealtorDto>>> GetRealtorsByAgencyCommun(string communName)
+        public async Task<ActionResult<List<RealtorSummaryDto>>> GetRealtorsByAgencyCommun(string communName)
         {
             var realtors = await _realtorRepository.GetRealtorsByAgencyCommunName(communName);
             if (realtors == null)
                 return NotFound();
-            var response = _mapper.Map<List<RealtorDto>>(realtors);
+            var response = _mapper.Map<List<RealtorSummaryDto>>(realtors);
             return Ok(response);
         }
 
