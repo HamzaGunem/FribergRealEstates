f25dac9dd77721fb93689245feb39768a8c2d181
JonteYrkesAkademin <jonathan.sorgjerd@yaelev.se>
2025-05-05 15:30:18 +0200

    Added POST-Endpoint for creating realtor

---
+++ FribergRealEstatesAPI/Data/Dto/RealtorCreateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FribergRealEstatesAPI.Data.Dto
+{
+
+    //Auth: Jonathan
+    public class RealtorCreateDto
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string? PictureUrl { get; set; }
+        public int AgencyId { get; set; }
+    }
+}
