8e4200e1248c6c3e79777140a6013320d18ba538
Robert Johansson <Wizti@users.noreply.github.com>
2025-05-06 16:36:57 +0200

    added class adminrealtorUserDto

---
+++ FribergRealEstatesAPI/Data/Dto/AdminRealtorUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FribergRealEstatesAPI.Data.Dto
+{
+    // Auth Robert
+    public class AdminRealtorUserDto
+    {
+        public int RealtorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string ApiUserId { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
