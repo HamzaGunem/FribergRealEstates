75546af25e980aaca5fda8dc64d0f372e4e332c9
SamAndrs <sam_andrs@yahoo.se>
2025-05-12 11:41:31 +0200

    Updated controller for creating Residence

--- FribergRealEstatesAPI/Controllers/ResidenceController.cs
+++ FribergRealEstatesAPI/Controllers/ResidenceController.cs
@@ -2,8 +2,11 @@
 using FribergRealEstatesAPI.Data.Dto;
 using FribergRealEstatesAPI.Data.Interfaces;
 using FribergRealEstatesAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Policy;
 
 namespace FribergRealEstatesAPI.Controllers
 {
@@ -90,22 +93,42 @@ namespace FribergRealEstatesAPI.Controllers
 
             return Ok(mapper.Map<ResidenceSummaryDto>(residence));
         }
-        //Auth: Oscar
-        [HttpPost]
-        public async Task<ActionResult<CreateResidenceDto>> CreateResidence(CreateResidenceDto dto)
-        {
-            var commun = await communRepository.GetByIdAsync(dto.CommunId);
-            if (commun == null)
-                return NotFound("Kommun hittades inte.");
-            var address = new Address
-            {
-                Street = dto.Street,
-                PostalCode = dto.PostalCode,
-                City = dto.City,
-                CommunId = dto.CommunId
-            };
-            await addressRepository.AddAsync(address);
 
+
+        //Auth: Oscar, modified by Samuel
+        [HttpPost]
+        [Authorize(Roles = "SuperAdmin")]
+        //public async Task<ActionResult<CreateResidenceDto>> CreateResidence(CreateResidenceDto dto)
+        public async Task<ActionResult> CreateResidence(CreateResidenceDto dto)
+        {
+            var newAddress = AddNewAddress(dto);
+
+            /*newAddress.Commun = await communRepository.GetByIdAsync(newAddress.CommunId);
+            
+            if (newAddress.Commun == null)
+                return NotFound("Kommun hittades inte.");*/
+
+            await addressRepository.AddAsync(newAddress);
+
+            Residence newResidence = MapNewResidence(dto, newAddress);
+
+            await _residenceRepository.AddAsync(newResidence);
+
+            return Ok(newResidence.Id);
+
+            /*
+           var commun = await communRepository.GetByIdAsync(dto.CommunId);
+           if (commun == null)
+               return NotFound("Kommun hittades inte.");
+           var address = new Address
+           {
+               Street = dto.Street,
+               PostalCode = dto.PostalCode,
+               City = dto.City,
+               CommunId = dto.CommunId
+           };
+           //await addressRepository.AddAsync(address);
+          
             Residence residence;
             try
             {
@@ -126,12 +149,13 @@ namespace FribergRealEstatesAPI.Controllers
             residence.AddressId = address.Id;
             residence.Address = address;
             residence.IsAvailable = true;
-
+            
             await _residenceRepository.AddAsync(residence);
             var residenceDto = mapper.Map<ResidenceDto>(residence);
             return CreatedAtAction(nameof(GetResidenceById), new { residenceId = residence.Id }, residenceDto);
-
+            */
         }
+
         //Auth: Oscar
         [HttpPut("{residenceId}")]
         public async Task<ActionResult<UpdateResidenceDto>> UpdateResidence(int residenceId, UpdateResidenceDto dto)
@@ -145,5 +169,59 @@ namespace FribergRealEstatesAPI.Controllers
             var updatedDto = mapper.Map<ResidenceDto>(residence);
             return Ok(updatedDto);
         }
-    }
+
+        // Samuel
+        private Address AddNewAddress(CreateResidenceDto dto)
+        {
+            Address newAddress = new()
+            {
+                Street = dto.Street,
+                PostalCode = dto.PostalCode,
+                City = dto.City,
+                CommunId = dto.CommunId,
+            };
+            return newAddress;
+        }// End
+
+        // Samuel
+        private Residence MapNewResidence(CreateResidenceDto dto, Address address)
+        {
+            //Residence newResidence = mapper.Map<Residence>(dto);
+            Residence newResidence = new()
+            {
+                Type = dto.Type,
+                Area = dto.Area,
+                BiArea = dto.BiArea,
+                Description = dto.Description,
+                Rooms = dto.Rooms,
+                Floors = dto.Floors,
+                FloorRows = dto.FloorRows,
+                MonthlyFee = dto.MonthlyFee,
+                OperatingCost = dto.OperatingCost,
+                BuildYear = dto.BuildYear,
+                ImageUrls = dto.ImageUrls,
+                ParkingSlotNumber = dto.ParkingSlotNumber,
+                IsAvailable = true,
+                Address = address,
+                AddressId = address.Id,
+                Facilities = dto.Facilities
+            };
+            
+            return newResidence;
+        }        
+    }// End
+
+        /*
+        private List<Facilities> MapFacilities(CreateResidenceDto dto)
+        {
+            List<Facilities> newList = new();
+            foreach(var facility in dto.Facilities)
+            {
+                if(facility == "Elevator") { }
+
+            }
+            // // Elevator, Balcony, Terrace, ParkingLot, NewProduction, Pool
+        }// End 
+        */
+    
 }
